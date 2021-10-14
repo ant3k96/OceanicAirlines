218752001a478855f7c9841b34a75230f029a59f
Bartłomiej Pieróg <bapi@netcompany.com>
2021-10-14 11:58:09 +0200

    Weight added

--- WebApplication1/OceanicAirlines.Domain/EntityModels/Route.cs
+++ WebApplication1/OceanicAirlines.Domain/EntityModels/Route.cs
@@ -19,6 +19,8 @@ namespace OceanicAirlines.Domain.EntityModels
         public Guid ToId { get; set; }
         [Required]
         public Size PackageSize { get; set; }
+        [Required]
+        public Weight PackageWeight { get; set; }
         public IEnumerable<City> GoThroughLocations { get; set; }
     }
 }
