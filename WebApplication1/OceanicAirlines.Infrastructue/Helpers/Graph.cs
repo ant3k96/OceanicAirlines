63b053f128862e0cb3e20a1138eac81907e792ea
Bartłomiej Pieróg <46139140+pierozkitwarozki@users.noreply.github.com>
2021-10-14 19:02:31 +0200

    Code Cleanup

---
+++ WebApplication1/OceanicAirlines.Infrastructue/Helpers/Graph.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace OceanicAirlines.Infrastructue.Helpers
+{
+    public class Graph
+    {
+        public List<SingleConnection> Rows { get; set; }
+        public string VehicleType { get; set; }
+    }
+}
---
+++ WebApplication1/OceanicAirlines.Infrastructue/Helpers/SingleConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace OceanicAirlines.Infrastructue.Helpers
+{
+    public class SingleConnection
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public double Weight { get; set; }
+    }
+}
