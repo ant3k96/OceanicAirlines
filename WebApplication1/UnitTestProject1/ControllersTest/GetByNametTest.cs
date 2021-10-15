2b81d7d65982f69d11e1807daeba899ffb0452a9
Antoni Kistowski <antoni.kistowski@gmail.com>
2021-10-15 11:49:11 +0200

    test method

---
+++ WebApplication1/UnitTestProject1/ControllersTest/GetByNametTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OceanicAirlines.Domain.EntityModels;
+using WebApplication1.Controllers;
+using OceanicAirlines.Application.Repos;
+using OceanicAirlines.Application.Services;
+using OceanicAirlines.Infrastructue.Services;
+using Moq;
+
+namespace UnitTestProject1.ControllersTest
+{
+    [TestClass]
+    public class GetByNametTest
+    {
+        public GetByNametTest()
+        {
+            cityService = new CityService(cityRepo.Object);
+        }
+        private Mock<ICityRepo> cityRepo = new Mock<ICityRepo>();
+
+        public CityService cityService { get; }
+
+        [TestMethod]
+        public void CityGetSingleName()
+        {
+            cityRepo.Setup(p => p.GetSingleByName("tanger")).Returns(new City {Name = "tanger"});
+
+            var result = cityService.GetSingleByName("tanger");
+
+            Assert.AreEqual("tanger", result.Name);
+        }
+    }
+}
---
+++ WebApplication1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cda7d46d-827c-49a7-970b-e2aec7adb239")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
